5e035a9225f56595aabd3d8cb25b167cf4f19967
BrewDrinkCode <scott@gscottsingleton.com>
2019-04-25 01:03:22 -0400

    add support for EIO's power manager and reaching back to get capacitor bank stats when OC adapter is only on a conduit and not actually up against a capacitor.

--- README.md
+++ README.md
@@ -13,33 +13,48 @@ Out of the box if you connect an OC Adapter to a large multi-block EnderIO capac
 get the basic energy_device adapter and that is limited to the actual capacitor the adapter is
 connected to and not the entire array of capacitors.
 
+
+**version 1.0.002**
+
 ## Supported Devices ##
 
-Conduits are not yet in the published version on CurseForge!
 
-~~### Ender IO Power Conduit with Capacitor Bank on same conduit line ###
+~~### Ender IO Conduit Bundles###
 
-This works the same way the EnderIO Power monitors do.  It requires that you use an EnderIO power conduit
+Bundles currently supports working with power and item conduits in the same bundle.  Simply put the adapter adjacent to
+a conduit bundle -- though you won't see it actually connect it can still interact with it.
+
+Power: This works the same way the EnderIO Power monitors do.  It requires that you use an EnderIO power conduit
 of any tier, and have that conduit attached to an EnderIO capacitor or array of capacitors.
 
-The conduit will not attach to the Adapter, simply make sure the adapter is in an adjacent block.
+Item: this allows you to get/set the extraction redstone mode as well as the channel color.  Do note that 
+EnderIO seems to use different visual colors than the enumerator returns.  For example, the RS signal channel will return
+'ORANGE' even though it really is red.  Totally weird.  Setting it to 'ORANGE' will make the signal channel red.  Don't ask me :P 
+I didn't write EIO :)
 
 **Make sure that you check the doesBundleContainPowerConduit() method first to ensure that the bundle 
 still has a power conduit in it, otherwise the API will fail, albeit gracefully. (hopefully)**
 
--  **doesBundleContainPowerConduit**=function():bool; Returns true if the bundle contains a power conduit,
--  **getAverageInputPerTick**=function():number; Returns the average input per Tick,
--  **getAverageOutputPerTick**=function():number; Returns the average output per tick across this conduit bundle,
--  **getBundleConduits**=function():table -- Returns a table of all conduits in bundle,
--  **getCurrentEnergyStored**=function():number; Returns Current Energy Stored,
--  **getExtractionRedstoneMode**=function(string EnumFacing) : string - redstone control mode for given side of power conduit,
--  **getIntegrationProvider**=function():string;  -- Returns name of mod providing integration. Used for validation that mod is installed prior to calling these functions.,
--  **getMaxEnergyStored**=function():number; Returns Max Energy Stored,
--  **getMaxPowerInConduits**=function():number; Returns the maximum power that can reside in the conduits,
--  **getMaxPowerInReceptors**=function():number; Returns the maximum power that can reside in power receptors on this network,
--  **getPowerInConduits**=function():number; Returns the current power stored in the conduits,
--  **getPowerInReceptors**=function():number; Returns the current power stored in the receptors,
--  **setExtractionRedstoneMode**=function(string RedstoneControlMode, string EnumFacing): {number theResult, string message} sets redstone control mode for given face,~~
+ - getBundleConduits=function():table -- Returns a table of all conduits in bundle,
+ - getIntegrationProvider=function():string;  -- Returns name of mod providing integration. Used for validation that mod is installed prior to calling these functions.,
+ 
+ - item_hasConduit=fuction():bool; Returns tru if the bundle contains an item conduit,
+ - item_getExtractionRedstoneMode=function(string RedstoneControlMode, string EnumFacing): {number,message} sets redstone control mode for given face,
+ - item_getExtractionSignalColor=function(string EnumFacing : string Dye Color,
+ - item_setExtractionRedstoneMode=function(string RedstoneControlMode, string EnumFacing): {number:,message} sets redstone control mode for given face,
+ - item_setExtractionSignalColor=function(string enumfacing, string DyeColor) returns result code and description,  0/OK means success,
+ 
+ - pwr_hasConduit=function():bool; Returns true if the bundle contains a power conduit,
+ - pwr_getAverageInputPerTick=function():number; Returns the average input per Tick,
+ - pwr_getAverageOutputPerTick=function():number; Returns the average output per tick across this conduit bundle,
+ - pwr_getCurrentEnergyStored=function():number; Returns Current Energy Stored,
+ - pwr_getExtractionRedstoneMode=function(string EnumFacing) : string - redstone control mode for item extraction on the given face,
+ - pwr_getMaxEnergyStored=function():number; Returns Max Energy Stored,
+ - pwr_getMaxPowerInConduits=function():number; Returns the maximum power that can reside in the conduits,
+ - pwr_getMaxPowerInReceptors=function():number; Returns the maximum power that can reside in power receptors on this network,
+ - pwr_getPowerInConduits=function():number; Returns the current power stored in the conduits,
+ - pwr_getPowerInReceptors=function():number; Returns the current power stored in the receptors,
+ - pwr_setExtractionRedstoneMode=function(string RedstoneControlMode, string EnumFacing): {number,string} sets redstone control for item extraction on the given face,
 
 
 ### Ender IO Capacitor Banks ###
--- src/main/java/com/brewingcoder/eioocshim/eio/DriverConduit.java
+++ src/main/java/com/brewingcoder/eioocshim/eio/DriverConduit.java
@@ -2,10 +2,11 @@ package com.brewingcoder.eioocshim.eio;
 
 import com.brewingcoder.eioocshim.EIOOCShim;
 import com.brewingcoder.eioocshim.oc.ManagedTileEnvironment;
-import crazypants.enderio.base.conduit.IConduit;
+import com.enderio.core.common.util.DyeColor;
 import crazypants.enderio.base.conduit.IConduitNetwork;
 import crazypants.enderio.base.machine.modes.RedstoneControlMode;
 import crazypants.enderio.conduits.conduit.TileConduitBundle;
+import crazypants.enderio.conduits.conduit.item.ItemConduit;
 import crazypants.enderio.conduits.conduit.power.NetworkPowerManager;
 import crazypants.enderio.conduits.conduit.power.PowerConduit;
 import crazypants.enderio.conduits.conduit.power.PowerConduitNetwork;
@@ -41,8 +42,70 @@ public class DriverConduit extends EIODriver<TileConduitBundle> {
             return new Object[]{tile.getConduits()};
         }
 
+        private ItemConduit GetItemConduit(){
+            return tile.getConduit(ItemConduit.class);
+        }
+
+        @Callback(doc = "fuction():bool; Returns tru if the bundle contains an item conduit")
+        public Object[] item_hasConduit(Context ctx, Arguments args){
+            return new Object[]{GetItemConduit() != null};
+        }
+
+        @Callback (doc="function(string RedstoneControlMode, string EnumFacing): {number:result,string:message} sets redstone control mode for given face")
+        public Object[] item_getExtractionRedstoneMode(Context ctx, Arguments args){
+            ItemConduit ic = GetItemConduit();
+            return (ic != null)
+                    ? new Object[]  {ic.getExtractionRedstoneMode(EnumFacing.valueOf(args.checkString(0)))}
+                    : new Object[] {null};
+        }
+
+        @Callback (doc="function(string RedstoneControlMode, string EnumFacing): {number:result,string:message} sets redstone control mode for given face")
+        public Object[] item_setExtractionRedstoneMode(Context ctx, Arguments args){
+            ItemConduit ic = GetItemConduit();
+            if(ic != null){
+                try{
+                    ic.setExtractionRedstoneMode(
+                            RedstoneControlMode.valueOf(args.checkString(0)),
+                            EnumFacing.valueOf(args.checkString(1)));
+                    return new Object[] {0,"OK"};
+                }catch(Exception e)
+                {
+                    return new Object[]{-1, e.getMessage()};
+                }
+            }
+            return new Object[] { -1,"Was Null"};
+        }
+
+        @Callback (doc="function(string EnumFacing : string Dye Color")
+        public Object[] item_getExtractionSignalColor(Context ctx, Arguments args)
+        {
+            ItemConduit ic = GetItemConduit();
+            return (ic != null)
+                    ? new Object[] {(ic.getExtractionSignalColor(EnumFacing.valueOf(args.checkString(0))))}
+                    : new Object[] {null};
+        }
+
+        @Callback (doc="function(string enumfacing, string DyeColor) returns result code and description,  0/OK means success")
+        public Object[] item_setExtractionSignalColor(Context ctx, Arguments args)
+        {
+            ItemConduit ic = GetItemConduit();
+            if(ic != null){
+                try{
+                    ic.setExtractionSignalColor(EnumFacing.valueOf(args.checkString(0)), DyeColor.valueOf(args.checkString(1)));
+                    return new Object[] {0,"OK"};
+                }catch(Exception e)
+                {
+                    return new Object[] {-1, e.getMessage()};
+                }
+            }
+            return new Object[] {-1, "Was Null"};
+        }
+
+        private PowerConduit GetPowerConduit(){
+            return tile.getConduit(PowerConduit.class);
+        }
         private NetworkPowerManager GetNetworkPowerManager(){
-            PowerConduit pc = tile.getConduit(PowerConduit.class);
+            PowerConduit pc = GetPowerConduit();
             if (pc != null) {
                 IConduitNetwork<?, ?> n = pc.getNetwork();
                 if (n instanceof PowerConduitNetwork) {
@@ -51,22 +114,26 @@ public class DriverConduit extends EIODriver<TileConduitBundle> {
             }
             return null;
         }
-
         private PowerTracker GetNetworkPowerTracker(){
             NetworkPowerManager pm = GetNetworkPowerManager();
             return pm != null ? pm.getNetworkPowerTracker() : null;
         }
 
-        @Callback (doc="function(string EnumFacing) : string - redstone control mode for given side of power conduit")
-        public Object[] getExtractionRedstoneMode(Context ctx, Arguments args){
+        @Callback(doc = "function():bool; Returns true if the bundle contains a power conduit")
+        public Object[] pwr_hasConduit(Context context, Arguments args){
+            return new Object[]{GetPowerConduit() != null};
+        }
+
+        @Callback (doc="function(string EnumFacing) : string - redstone control mode for item extraction on the given face")
+        public Object[] pwr_getExtractionRedstoneMode(Context ctx, Arguments args){
             PowerConduit pc = tile.getConduit(PowerConduit.class);
             return (pc != null)
                 ? new Object[] {pc.getExtractionRedstoneMode(EnumFacing.valueOf(args.checkString(0)))}
                 : new Object[] {null};
         }
 
-        @Callback (doc="function(string RedstoneControlMode, string EnumFacing): {number:result,string:message} sets redstone control mode for given face")
-        public Object[] setExtractionRedstoneMode(Context ctx, Arguments args){
+        @Callback (doc="function(string RedstoneControlMode, string EnumFacing): {number:result,string:message} sets redstone control for item extraction on the given face")
+        public Object[] pwr_setExtractionRedstoneMode(Context ctx, Arguments args){
             PowerConduit pc = tile.getConduit(PowerConduit.class);
             if (pc != null){
                 try {
@@ -83,9 +150,8 @@ public class DriverConduit extends EIODriver<TileConduitBundle> {
             return new Object[] {-1,"Was Null"};
         }
 
-
         @Callback (doc="function():number; Returns the maximum power that can reside in the conduits")
-        public Object[] getMaxPowerInConduits(Context ctx, Arguments args){
+        public Object[] pwr_getMaxPowerInConduits(Context ctx, Arguments args){
             NetworkPowerManager pm = GetNetworkPowerManager();
             return (pm != null)
                     ? new Object[]{pm.getMaxPowerInConduits()}
@@ -93,7 +159,7 @@ public class DriverConduit extends EIODriver<TileConduitBundle> {
         }
 
         @Callback (doc="function():number; Returns the current power stored in the conduits")
-        public Object[] getPowerInConduits(Context ctx, Arguments args){
+        public Object[] pwr_getPowerInConduits(Context ctx, Arguments args){
             NetworkPowerManager pm = GetNetworkPowerManager();
             return (pm != null)
                     ? new Object[]{pm.getPowerInConduits()}
@@ -101,7 +167,7 @@ public class DriverConduit extends EIODriver<TileConduitBundle> {
         }
 
         @Callback (doc="function():number; Returns the maximum power that can reside in power receptors on this network")
-        public Object[] getMaxPowerInReceptors(Context ctx, Arguments args){
+        public Object[] pwr_getMaxPowerInReceptors(Context ctx, Arguments args){
             NetworkPowerManager pm = GetNetworkPowerManager();
             return (pm != null)
                     ? new Object[]{pm.getMaxPowerInReceptors()}
@@ -109,7 +175,7 @@ public class DriverConduit extends EIODriver<TileConduitBundle> {
         }
 
         @Callback (doc="function():number; Returns the current power stored in the receptors")
-        public Object[] getPowerInReceptors(Context ctx, Arguments args){
+        public Object[] pwr_getPowerInReceptors(Context ctx, Arguments args){
             NetworkPowerManager pm = GetNetworkPowerManager();
             return (pm != null)
                     ? new Object[]{pm.getPowerInReceptors()}
@@ -117,7 +183,7 @@ public class DriverConduit extends EIODriver<TileConduitBundle> {
         }
 
         @Callback (doc="function():number; Returns the average output per tick across this conduit bundle")
-        public Object[] getAverageOutputPerTick(Context ctx, Arguments args){
+        public Object[] pwr_getAverageOutputPerTick(Context ctx, Arguments args){
             PowerTracker pt = GetNetworkPowerTracker();
             return (pt != null)
                     ? new Object[]{pt.getAverageRfTickSent()}
@@ -125,7 +191,7 @@ public class DriverConduit extends EIODriver<TileConduitBundle> {
         }
 
         @Callback (doc="function():number; Returns the average input per Tick")
-        public Object[] getAverageInputPerTick(Context ctx, Arguments args){
+        public Object[] pwr_getAverageInputPerTick(Context ctx, Arguments args){
             PowerTracker pt = GetNetworkPowerTracker();
             return (pt != null)
                     ? new Object[]{pt.getAverageRfTickRecieved()}
@@ -133,7 +199,7 @@ public class DriverConduit extends EIODriver<TileConduitBundle> {
         }
 
         @Callback(doc = "function():number; Returns Max Energy Stored")
-        public Object[] getMaxEnergyStored(Context ctx, Arguments args){
+        public Object[] pwr_getMaxEnergyStored(Context ctx, Arguments args){
             NetworkPowerManager pm = GetNetworkPowerManager();
             return (pm != null)
                     ? new Object[]{pm.getMaxPowerInCapacitorBanks()}
@@ -141,21 +207,12 @@ public class DriverConduit extends EIODriver<TileConduitBundle> {
         }
 
         @Callback(doc = "function():number; Returns Current Energy Stored")
-        public Object[] getCurrentEnergyStored(Context ctx, Arguments args){
+        public Object[] pwr_getCurrentEnergyStored(Context ctx, Arguments args){
             NetworkPowerManager pm = GetNetworkPowerManager();
             return (pm != null)
                     ? new Object[]{pm.getPowerInCapacitorBanks()}
                     : new Object[]{null};
         }
 
-        @Callback(doc = "function():bool; Returns true if the bundle contains a power conduit")
-        public Object[] doesBundleContainPowerConduit(Context context, Arguments args){
-            for (IConduit con : tile.getConduits()){
-                if(con.getClass().toString().contains("AbstractConduit")){
-                    return new Object[]{true};
-                }
-            }
-            return new Object[]{false};
-        }
     }
 }
